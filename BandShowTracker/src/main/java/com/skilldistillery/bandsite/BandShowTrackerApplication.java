4d20443ff68b9037c84a91ad9c4e61172149a0ac
mjingram <micha.j.ingram@gmail.com>
2021-07-30 17:09:45 -0500

    add Jpa and Boot

---
+++ BandShowTracker/src/main/java/com/skilldistillery/bandsite/BandShowTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bandsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandShowTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandShowTrackerApplication.class, args);
+	}
+
+}
---
+++ JPABandSite/src/test/java/com/skilldistillery/bandsite/entities/ConcertTest.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.bandsite.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ConcertTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Concert concert;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPABandSite");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+		}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	em = emf.createEntityManager();
+	concert = em.find(Concert.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		concert = null;
+	}
+
+	@Test
+	@DisplayName("Concert Entity Mapping")
+	void test1() {
+		assertNotNull(concert);
+		assertEquals("First Show", concert.getName());
+	}
+
+}
