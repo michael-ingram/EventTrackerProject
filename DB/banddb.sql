d4051b4fcc0afb2dfc327cde04c5e23529ec0826
mjingram <micha.j.ingram@gmail.com>
2021-07-30 15:48:58 -0500

    Create DB schema

---
+++ DB/banddb.sql
@@ -0,0 +1,48 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema banddb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `banddb` ;
+
+-- -----------------------------------------------------
+-- Schema banddb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `banddb` DEFAULT CHARACTER SET utf8 ;
+USE `banddb` ;
+
+-- -----------------------------------------------------
+-- Table `show`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `show` ;
+
+CREATE TABLE IF NOT EXISTS `show` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS banduser@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'banduser'@'localhost' IDENTIFIED BY 'banduser';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'banduser'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `show`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `banddb`;
+INSERT INTO `show` (`id`, `name`) VALUES (1, 'First Show');
+
+COMMIT;
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # EventTrackerProject
+
+### Full-Stack Spring/REST/JPA EventTrackerProject
+
+## Overview
+* TODO: description
+
+## REST Endpoints
+
+| Method | URI                    | Request Body | Response Body | Purpose |
+|--------|------------------------|--------------|---------------|---------|
